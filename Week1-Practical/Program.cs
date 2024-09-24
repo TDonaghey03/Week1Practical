2018f751a409e6875549c29e8242d22f1c22e252
csf23dot <csf23dot@MF124PC77>
2024-09-24 15:50:34 +0100

    Add project files.

---
+++ Week1-Practical/Program.cs
@@ -0,0 +1,77 @@
+﻿//Task 1:
+//This asks for a users name and age and outputs what age they'll be in 5 years time
+//Console.WriteLine("Please enter your name:"); //Asks user to enter name
+//string Name = Console.ReadLine();  
+//Console.WriteLine("Please enter your age:"); //Asks user to enter age
+//int Age = Convert.ToInt32(Console.ReadLine());
+//int result = Age + 5; //Calculates what age they will be in 5 years time
+//Console.WriteLine("Hello {0} you will be {1} in 5 years", Name, result); 
+
+//Task 2:
+//This asks for a users age and outputs if they are a child, teenager or adult
+//Console.WriteLine("Please enter your age:");
+//int Age = Convert.ToInt32(Console.ReadLine());
+//if (Age > 0 && Age < 13)
+//{
+//    Console.WriteLine("You are a child");
+//}
+//else if (Age < 0)
+//{
+//    Console.WriteLine("Invalid age - age must be above 0");
+//}
+//else if (Age > 12 && Age < 20)
+//{
+//    Console.WriteLine("You are a teenager");
+//}
+//else if (Age > 19 && Age < 110)
+//{
+//    Console.WriteLine("You are an adult");
+//}
+//else if (Age > 110)
+//{
+//    Console.WriteLine("Age exceeds limit");
+//}
+
+//Task 3:
+//This asks for a number and will output if the number is negative, positive or zero
+//Console.WriteLine("Please enter a number");
+//int Number = Convert.ToInt32(Console.ReadLine());
+//if (Number > 0)
+//{
+//    Console.WriteLine("Your number is positive");
+//}
+//else if (Number < 0)
+//{
+//    Console.WriteLine("Your number is negative");
+//}
+//else if (Number == 0)
+//{
+//    Console.WriteLine("Your number is 0");
+//}
+
+//Task 4:
+//This checks if a given year is a leap year
+//Console.WriteLine("Please enter a year");
+//int Year = Convert.ToInt32(Console.ReadLine());
+//int divisor = 4;
+//int remainder = Year % divisor;
+//if (remainder == 0)
+//{
+//    Console.WriteLine("This year is a leap year");
+//}
+//else if (remainder != 0)
+//{ 
+//    Console.WriteLine("This year is not a leap year");
+//}
+
+//Task 5:
+//Console.WriteLine("Please enter an integer");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number % 2 == 0)
+//{
+//    Console.WriteLine($"{number} is an even number.");
+//else
+//{
+//    Console.WriteLine($"{number} is an odd number.");
+//}
+
